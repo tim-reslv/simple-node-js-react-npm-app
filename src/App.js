bd6a3481dd81f6f3dfac6930d95af473c2838d9a
tim-reslv <64763691+tim-reslv@users.noreply.github.com>
2021-09-29 10:00:23 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-          test 4
+          test 5
         </p>
       </div>
     );
